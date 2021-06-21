9c5304d9f90e3dc9cba7e4cc3aabc1b9daa10c4e
M.kamal <33860580+kamal-desgin@users.noreply.github.com>
2021-06-21 16:57:46 +0530

    add
    add

--- tshirt-onlinesale/projfrontend/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Routes from "./Routes";
-import ReactDom from 'react-dom';
-
-ReactDom.render(<Routes />, document.getElementById("root"));
--- tshirt-onlinesale/testfront/myrouter/src/Visit.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Visit() {
-  return (
-    <div>
-      <h1> Visit Component</h1>
-    </div>
-  )
-}
